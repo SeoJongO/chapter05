0cfb87d4e9b2102d63750f6cfe6ad7a381a6e37d
SeoJongO <eve7884dbqls@naver.com>
2021-06-17 11:20:15 +0900

    0617 2교시

---
+++ src/com/javaex/ex02/Ex03.java
@@ -0,0 +1,16 @@
+package com.javaex.ex02;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Ex03 {
+
+	public static void main(String[] args) throws IOException {
+
+		InputStream in = new FileInputStream("C:\\Users\\Hi-PC\\Desktop\\JavaStudy\\file\\MS949.txt");
+		
+		
+	}
+
+}
